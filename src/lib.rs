962f864d61fb699ea1dfa44af32c36dfe497a096
amirography <amirhossein.alesheikh@gmail.com>
2022-12-03 14:23:37 +0330

    build: moving to nix flake for ci

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,16 @@ pub mod routes;
 pub mod startup;
 
 use axum::{self, http, routing, Router, Server};
+use sqlx::PgConnection;
 use tokio::net::TcpListener;
 
-pub async fn run(listener: TcpListener) -> Result<(), std::io::Error> {
+pub async fn run(listener: TcpListener, connection: PgConnection) -> Result<(), std::io::Error> {
     // tracing_subscriber::fmt::init();
 
     let app = Router::new()
         .route("/health_check", routing::get(routes::health_check))
-        .route("/subscriptions", routing::post(routes::subscribe));
+        .route("/subscriptions", routing::post(routes::subscribe))
+        .with_state(connection);
 
     Server::from_tcp(listener.into_std().expect("problem converting"))
         .expect("shit")
